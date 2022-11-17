d53a40d1f7b1bfb7301b7cd1f18339ca1275f770
junkaichen <junkaichenalex@gmail.com>
2022-11-17 14:32:42 -0700

    rebuild Project

--- Source/Elevator_Stealth/Elevator_Stealth.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Elevator_Stealth.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Elevator_Stealth, "Elevator_Stealth" );
- 
